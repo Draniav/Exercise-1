1e80db4acf0a475f7c88ade37db6b81e87bd22f4
Alexander Reyes <daniel.alexander.reyes@gmail.com>
2022-07-07 20:42:04 -0500

    initial  menu  created
    >cases for do  the navigation  between  the different classes.

--- src/VetPet/Main.java
+++ src/VetPet/Main.java
@@ -2,16 +2,38 @@ package VetPet;
 
 import java.util.Arrays;
 
+
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to VetPet's App");
-        Messages.menuMain();
-        Messages.menuMenu();
+       Messages.menuMain();
+    //    Messages.menuMenu();
 
+        //input by  keyboard the option , the app reads in  the variable "menu"
+        int menu = 1;
 
-
+        switch(menu){
+            case 1:
+                Messages.menuInventory();
+                break;
+            case 2:
+                Messages.menuSchedule();
+                break;
+            case 3:
+                Messages.menuEmployees();
+                break;
+            case 4:
+                Messages.menuPatients();
+                break;
+            case 0:
+                System.exit(0);
+                break;
+            default:
+                break;
+        }
 
 
 
