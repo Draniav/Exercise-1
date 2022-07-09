888395a988e59795c942bdc9967bc29b41698c53
Julian Duque <91387382+JulianDuque97@users.noreply.github.com>
2022-07-09 14:36:25 -0500

    Update Inventory_control
    > Renamed the function.

--- src/VetPet/Inventory/Inventory_Control.java
+++ src/VetPet/Inventory/Inventory_Control.java
@@ -4,13 +4,12 @@ import java.util.ArrayList;
 
 public class Inventory_Control {
 
-    public ArrayList upLoadSongsLibrary(ArrayList<Inventory> myInventoryList) {
+    public ArrayList upLoadInventaryList(ArrayList<Inventory> myInventoryList) {
 
 
         myInventoryList.add(new Inventory(0, "ibuprofen      ", "Pill", "Human", 8, 3));
-        myInventoryList.add(new Inventory(0, "maxiperro      ", "Pet_care", "Veterinarian", 5, 1
+        myInventoryList.add(new Inventory(0, "maxiperro      ", "Pet_care", "Veterinarian", 5, 1));
 
-        ));
 
         /**
          * Scrolls through the items in stock and changes the
