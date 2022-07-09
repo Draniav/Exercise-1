3c830f9472a5a61f5eca78070d9f2f5113147ed5
Julian Duque <91387382+JulianDuque97@users.noreply.github.com>
2022-07-09 14:59:59 -0500

    Update Inventory_Control.java
    >making tests with the branch

--- src/VetPet/Inventory/Inventory_Control.java
+++ src/VetPet/Inventory/Inventory_Control.java
@@ -10,6 +10,7 @@ public class Inventory_Control {
         myInventoryList.add(new Inventory(0, "ibuprofen      ", "Pill", "Human", 8, 3));
         myInventoryList.add(new Inventory(0, "maxiperro      ", "Pet_care", "Veterinarian", 5, 1));
         myInventoryList.add(new Inventory(0, "maxiperro      ", "Pet_care", "Veterinarian", 5, 1));
+        myInventoryList.add(new Inventory(0, "maxiperro      ", "Pet_care", "Veterinarian", 5, 1));
 
 
         /**
